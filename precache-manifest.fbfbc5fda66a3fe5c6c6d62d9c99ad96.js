77088543a25c17f8747519d44b36fcf09a9d0d4c
waterylime46@aol.com <waterylime46@aol.com>
2020-02-26 19:13:32 -0700

    Updates

---
+++ precache-manifest.fbfbc5fda66a3fe5c6c6d62d9c99ad96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c865685bc636c058a082db0fe793ae21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4e851cde8794a2bd231",
+    "url": "/static/css/main.6d84341f.chunk.css"
+  },
+  {
+    "revision": "af7a05b1e3a02f8aa719",
+    "url": "/static/js/2.cc7f98e7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.cc7f98e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e851cde8794a2bd231",
+    "url": "/static/js/main.ab7cce75.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "b6c7b732babf6092954ffe8c660847d3",
+    "url": "/static/media/IMG_0159.b6c7b732.JPG"
+  },
+  {
+    "revision": "20a64e481df586c124c18d009c8012a5",
+    "url": "/static/media/good.20a64e48.JPG"
+  }
+]);
